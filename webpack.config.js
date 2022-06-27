397c276ee62ad6ee7dc8492b6b681a3308af0685
GuanSzzz <847388139@qq.com>
2022-06-27 20:50:20 +0800

    完成了自动生成、删除、开启服务器

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,34 @@
 // 1.引入路径
 const path = require('path');
-// 配置路径
+
+// 2.引入自动生成html
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+// 3.引入自动删除
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+
+
 module.exports = {
-    entry:'./src/index.js',
-    output:{
-        path:path.resolve(__dirname, 'dist'),
-        filename:'bundle.js'
+    mode: 'development',
+    // 配置自动生成html
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: 'index.html'
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    // 配置路径
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    // 配置服务器端口
+    devServer: {
+        port: 3000, // 端口号
+        open: true
     }
 }
 
